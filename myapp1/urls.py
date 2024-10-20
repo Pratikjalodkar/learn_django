0d5bf84615ae095711788cbbeb3cf203b0ad2c70
Pratik Jalodkar <pratikjalodiya224@gmail.com>
2024-10-20 23:52:51 +0530

    second commit

--- myapp1/urls.py
+++ myapp1/urls.py
@@ -3,12 +3,13 @@ from django.urls import path
 from myapp1 import views
 
 urlpatterns = [
+    # path('', views.test, name="test"),
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
     path('services/', views.services, name="services"),
     path('contact/', views.contact, name="contact"), 
-    path('course/', views.course, name="course"), 
-    path('course/<courseId>/', views.courseDetails),
-    path('renderPage/', views.htmlRenderPage)  
+    # path('course/', views.course, name="course"), 
+    # path('course/<courseId>/', views.courseDetails),
+    # path('renderPage/', views.htmlRenderPage)  
 ]
 
--- myapp1/views.py
+++ myapp1/views.py
@@ -1,33 +1,36 @@
 from django.shortcuts import render, HttpResponse
 
+# def test(request):
+#     data ={
+#         'title':'Personnel Website',
+#         'htmldata':'Hii I am Pratik Jalodkar',
+#         'clist':['JAVA', 'Python', 'Django'],
+#         'student_details':[
+#             {'name':'Pratik', 'mobile_no':'1234567890'},
+#             {'name':'Pramod', 'mobile_no':'2356867890'},
+#         ]
+#     }
+#     return render(request, "index.html", data)
+
 def home(request):
-    data ={
-        'title':'Personnel Website',
-        'htmldata':'Hii I am Pratik Jalodkar',
-        'clist':['JAVA', 'Python', 'Django'],
-        'student_details':[
-            {'name':'Pratik', 'mobile_no':'1234567890'},
-            {'name':'Pramod', 'mobile_no':'2356867890'},
-        ]
-    }
-    return render(request, "index.html", data)
+    return render(request, "index.html")
 
 def about(request):
-    return HttpResponse("this is about page")
+    return render(request, "about.html")
 
 def services(request):
-    return HttpResponse("this is services page")
+    return render(request, "services.html")
 
 def contact(request):
-    return HttpResponse("this is contact page")
+    return render(request, "contact.html")
 
-def course(request):
-    return HttpResponse("this is course page")
+# def course(request):
+#     return HttpResponse("this is course page")
 
-def courseDetails(request,courseId):
-    return HttpResponse(f"<h1>{courseId}<h1/>")
+# def courseDetails(request,courseId):
+#     return HttpResponse(f"<h1>{courseId}<h1/>")
 
-def htmlRenderPage(request):
-    return render(request, "index.html")
+# def htmlRenderPage(request):
+#     return render(request, "index.html")
 
 
